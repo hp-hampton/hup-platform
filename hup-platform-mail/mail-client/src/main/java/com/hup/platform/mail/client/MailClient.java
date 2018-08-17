99c7b3999c7f6c55d7060956aa92866c1397f229
hup <hp_hampton@163.com>
2018-08-17 18:01:26 +0800

    spring boot（2.0.4） spring cloud（Finchley.SR1） 升级

--- hup-platform-mail/mail-client/src/main/java/com/hup/platform/mail/client/MailClient.java
+++ hup-platform-mail/mail-client/src/main/java/com/hup/platform/mail/client/MailClient.java
@@ -1,12 +1,12 @@
 package com.hup.platform.mail.client;
 
 import com.hup.platform.mail.model.MailRequest;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "")
+@FeignClient(name = "platform-mail")
 @RequestMapping(value = "/mail")
 public interface MailClient {
 
