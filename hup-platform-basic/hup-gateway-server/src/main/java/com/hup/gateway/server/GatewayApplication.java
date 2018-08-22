0aa9fdf61c70dca6ac4abcfe4aac0832e8221f1e
hup <hp_hampton@163.com>
2018-08-22 17:10:27 +0800

    spring boot（2.0.4） spring cloud（Finchley.SR1） 升级 -----zuul的改造

--- hup-platform-basic/hup-gateway-server/src/main/java/com/hup/gateway/server/GatewayApplication.java
+++ hup-platform-basic/hup-gateway-server/src/main/java/com/hup/gateway/server/GatewayApplication.java
@@ -2,11 +2,11 @@ package com.hup.gateway.server;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringCloudApplication
-@EnableOAuth2Sso
+@EnableZuulServer
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class,args);
