d9e6787e969efffcd80153702bbca04980782156
hup <hp_hampton@163.com>
2018-08-24 15:13:26 +0800

    spring boot（2.0.4） spring cloud（Finchley.SR1）网关服务：测试网关分发

--- hup-platform-basic/hup-gateway-server/src/main/java/com/hup/gateway/server/GatewayApplication.java
+++ hup-platform-basic/hup-gateway-server/src/main/java/com/hup/gateway/server/GatewayApplication.java
@@ -3,10 +3,8 @@ package com.hup.gateway.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringCloudApplication
-@EnableZuulServer
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class,args);
